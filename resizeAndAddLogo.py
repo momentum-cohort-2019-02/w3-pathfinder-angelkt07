91363ce4767d887bde1d4dab05229ba12d5a4c5b
Kasey Tiller <kntiller@ccicustom.com>
2019-02-28 14:29:18 -0500

    Pushing changes as of right now.

--- resizeAndAddLogo.py
+++
@@ -1,8 +0,0 @@
-#! python3
-# resizeAndAddLogo.py - resizes all images in  current working direction to fit in a 300x300 square, and adds catlogo.png to the lower-right corner.
-
-import os
-from PIL import images
-
-SQUARE_FIT_SIZE = 300
-LOGO_FILENAME = 'catlogo.png'
