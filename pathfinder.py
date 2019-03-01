b6718e2b8c6ab227145a7a159c892170268cade4
Kasey Tiller <kntiller@ccicustom.com>
2019-03-01 14:20:09 -0500

    Created a dictionary with points and color percentage.

--- pathfinder.py
+++ pathfinder.py
@@ -1,41 +1,69 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageColor
 import os
 
 
-class ElevationList:
+def get_elevation_lists(file_name):
+    """
+    given a file with elevation data, read each line and return a list for each line.
+    """
+    elevation_list = []
+    with open(file_name) as file:
+        for item in file:
+            elevation_list = [int((int(item)-3139)/(5648-3139) * 255) for item in file.read().split()]
 
-    def get_elevation_lists(file_name):
-        """
-        given a file with elevation data, read each line and return a list for each line.
-        """
+    return elevation_list
 
-        with open(file_name) as file:
-            elevation_list = [line.strip().lower() for line in file]
 
-        return elevation_list
 
-class CreateMap:
-    pass
-    # def create_image():
-    #     #TODO: the lab says the area of the mountians is 90m x 90m and the is 8100 meters according to Google. Not entirely sure how to make this into (top, left) = (0,0) and (bottom, right) = (??)
-    #     os.makedirs('forMap', exist_ok=True)
-
-    #     elevation_map = Image.new('RGBA', (90, 90))
-    #     elevation_map.getpixel((0, 0))
-
-    #     for x in range(100):
-    #         for y in range(50):
-    #             elevation_map.putpixel((x, y), (210, 210, 210))
+def get_map_image(mapIm):
+    elevation_list_junior = []
+    for left in range(0, width):
+        for top in range(0, height):
+            elevation_list_junior.append((left, top))
+            # print(left, top)
         
-    #     for x in range(100):
-    #         for y in range(50, 100):
-    #             elevation_map.putpixel((x, y), ImageColor.getcolor('darkgray', 'RGBA'))
-        
-    #     elevation_map.save(os.path.join('forMap','map.png'))
+    return elevation_list_junior
 
-    def create_map(elevation_list):
-        pass
+def get_dictionary(elevation_list_junior, elevation_list):
+    elevation_dictionary = dict(zip(elevation_list_junior, elevation_list))
+    return elevation_dictionary
+
+
+class MapImage:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 
 if __name__ == "__main__":
-    pass
+    file_name = 'elevation_small.txt'
+    mapIm = Image.new('RGBA', (600, 600), 'white')
+
+    width, height = mapIm.size
+
+    # print(get_elevation_lists(file_name))
+
+
+
+    # max_elevation = max([max(row) for row in get_elevation_lists(file_name)])
+    # min_elevation = min([min(row) for row in get_elevation_lists(file_name)])
+    # print(get_map_image(mapIm))
+    # print(min_elevation, max_elevation)
+    print(get_dictionary(get_map_image(mapIm), get_elevation_lists(file_name)))
+
+
+    mapIm.save('map.png')
+
+
+
+
+
+# # https://www.w3resource.com/python-exercises/file/python-io-exercise-9.php for how to figure out lines. I drew a blank and found enumerate. 
+# def file_lengthy(file_name):
+#     with open(file_name) as f:
+#             for items, line in enumerate(f):
+#                 pass
+#     return items + 1
+
+
--- test_pathfinder.py
+++ test_pathfinder.py
@@ -1,7 +1,14 @@
 from pathfinder import get_elevation_lists
+from PIL import Image
 
 
 def test_get_elevation_lists():
     run_file = get_elevation_lists('elevation_small.txt')
     assert type(run_file) == list
+
+
+
+# def test_get_map_image():
+#     run_image = get_map_image('RGBA', (600, 600))
+#     assert type(run_image) == list
 
